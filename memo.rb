7cb34074e897b47228072dfe6ab6451c238db476
USER <user@USERnoMacBook-Pro.local>
2023-11-24 17:41:01 +0900

    不要なカンマ等を修正

--- memo.rb
+++ memo.rb
@@ -1,26 +1,23 @@
-require "csv" # CSVファイルを扱うためのライブラリを読み込んでいます
+require "csv" 
 
 while true
     puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
-    memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換しています
-
+    memo_type = gets.to_i 
     if memo_type == 1 || memo_type == 2
         break
     else 
         puts "[エラー]1か2を入力してください"
     end
 end
-    # if文を使用して続きを作成していきましょう。
-    # 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
-
+   
 if memo_type == 1
     puts "拡張子を除いたファイル名を入力してください"
     file_name = gets.chomp
     puts "メモしたい内容を記入してください"
     puts "完了したら Ctrl + D を押します"
-    input_content = readlines
+    input_content = STDIN.read.chomp
     CSV.open("#{file_name}.csv", "w") do |csvmemo|
-        csvmemo << input_content
+        csvmemo << [input_content]
     end
 elsif memo_type == 2
     while true
@@ -36,8 +33,8 @@ elsif memo_type == 2
 
     puts "追記したい内容を記入してください"
     puts "完了したら Ctrl + D を押します"
-    input_content = readlines
+    input_content = STDIN.read.chomp
     CSV.open("#{file_name}.csv", "a") do |csvmemo|
-        csvmemo << input_content
+        csvmemo << [input_content]
     end
 end
